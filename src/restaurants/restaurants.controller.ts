5b0a6a0a1e3ec8f447d3706dbaa2fcd5454ffa8d
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2024-02-25 21:12:47 +0400

    feat: add getRestaurantById functionality

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './schemas/restaurant.schema';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
@@ -17,5 +17,13 @@ export class RestaurantsController {
     @Body() restaurant: CreateRestaurantDto,
   ): Promise<Restaurant> {
     return this.restaurantsService.create(restaurant);
+  }
+
+  @Get(':id')
+  async getRestaurant(
+    @Param('id')
+    id: string,
+  ): Promise<Restaurant> {
+    return this.restaurantsService.findById(id);
   }
 }
