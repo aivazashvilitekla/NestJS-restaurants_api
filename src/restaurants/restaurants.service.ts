fefa0283084824d15f96b3dd98d8428bd558ee68
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2024-02-25 20:54:36 +0400

    init: create project and restaurants module

---
+++ src/restaurants/restaurants.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
+import * as mongoose from 'mongoose';
+
+@Injectable()
+export class RestaurantsService {
+  constructor(
+    @InjectModel(Restaurant.name)
+    private restaurantModel: mongoose.Model<Restaurant>,
+  ) {}
+
+  // Get all Restaurants => GET /restaurants
+  async findAll(): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantModel.find();
+    return restaurants;
+  }
+
+  // Create new Restaurant => POST /restaurants
+  async create(restaurant: Restaurant): Promise<Restaurant> {
+    const res = await this.restaurantModel.create(restaurant);
+    return res;
+  }
+}
