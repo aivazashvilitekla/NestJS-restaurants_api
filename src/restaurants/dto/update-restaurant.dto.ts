caf9f0a06970b81ff17a26cf6dabb4ff69a5c25b
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2024-02-27 16:32:05 +0400

    add validations for updating the restaurant

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,10 +1,29 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schemas/restaurant.schema';
 
 export class UpdateRestaurantDto {
+  @IsString()
+  @IsOptional()
   readonly name: string;
+  @IsString()
+  @IsOptional()
   readonly description: string;
+  @IsEmail({}, { message: 'Please enter correct email address' })
+  @IsOptional()
   readonly email: string;
+  @IsPhoneNumber('GE')
+  @IsOptional()
   readonly phoneNo: number;
+  @IsString()
+  @IsOptional()
   readonly address: string;
+  @IsEnum(Category, { message: 'Please enter correct category' })
+  @IsOptional()
   readonly category: Category;
 }
